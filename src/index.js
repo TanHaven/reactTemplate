bcf52025adad35b319354fe019baab26122bb9dc
TanHaven <2582003996@qq.com>
2022-07-08 10:32:22 +0800

    react模板

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+//引入react核心库
+import React from "react";
+
+// 引入ReactDOM(React17)
+// import ReactDOM from "react-dom";
+// React18
+import { createRoot } from 'react-dom/client';
+
+// 引入App组件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 渲染App到页面(React17)
+// ReactDOM.render(<App/>,document.getElementById('root'))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// React18
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
